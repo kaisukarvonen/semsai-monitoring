4866e88b7d7522973577e431f7728447a1b6c801
kaisukarvonen <karvonenkaisu@gmail.com>
2019-12-10 13:48:46 +0200

    fix fonts

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -8,73 +8,77 @@ import {
   CardHeader,
   CardContent,
   Typography,
-  IconButton,
-}from '@material-ui/core';
-import CreateIcon from '@material-ui/icons/Create';
+  IconButton
+} from "@material-ui/core";
+import CreateIcon from "@material-ui/icons/Create";
 
 const Confirmation = () => {
   return (
     <>
       <ProgressBar />
-      <p>Make sure that all the information is correct before sending your answer</p>
-          <Card>
-            <CardHeader
-            action={
-              <IconButton color="primary" aria-label="edit problem" href="/">
+      <p>
+        Make sure that all the information is correct before sending your answer
+      </p>
+      <Card>
+        <CardHeader
+          action={
+            <IconButton color="primary" aria-label="edit problem" href="/">
               <CreateIcon />
-              </IconButton>
-            }
-            title ="Problem:"
-          />
-          <CardContent>
-            <Typography >
-              Text
-            </Typography>
-            </CardContent>
-          </Card>
-          <p></p>
-          <Card>
-            <CardHeader
-            action={
-              <IconButton color="primary" aria-label="edit scale" href="workers-affected">
+            </IconButton>
+          }
+          title="Problem:"
+        />
+        <CardContent>
+          <Typography>Text</Typography>
+        </CardContent>
+      </Card>
+      <p></p>
+      <Card>
+        <CardHeader
+          action={
+            <IconButton
+              color="primary"
+              aria-label="edit scale"
+              href="workers-affected"
+            >
               <CreateIcon />
-              </IconButton>
-            }
-            title ="Scale:"
-          />
-          </Card>
-          <p></p>
-          <Card>
-            <CardHeader
-            action={
-              <IconButton color="primary" aria-label="edit scale" href="location">
+            </IconButton>
+          }
+          title="Scale:"
+        />
+      </Card>
+      <p></p>
+      <Card>
+        <CardHeader
+          action={
+            <IconButton color="primary" aria-label="edit scale" href="location">
               <CreateIcon />
-              </IconButton>
-            }
-            title ="Location:"
-          />
-          <CardContent>
-            <Typography >
-              Text
-            </Typography>
-            </CardContent>
-          </Card>
-          <p></p>
-          <Card>
-            <CardHeader
-            action={
-              <IconButton color="primary" aria-label="edit info" href="/more-info">
+            </IconButton>
+          }
+          title="Location:"
+        />
+        <CardContent>
+          <Typography>Text</Typography>
+        </CardContent>
+      </Card>
+      <p></p>
+      <Card>
+        <CardHeader
+          action={
+            <IconButton
+              color="primary"
+              aria-label="edit info"
+              href="/more-info"
+            >
               <CreateIcon />
-              </IconButton>
-            }
-            title ="More information:"
-          />
-          <CardContent>
-            <Typography >
-              Text
-            </Typography>
-            </CardContent>
-          </Card>
+            </IconButton>
+          }
+          title="More information:"
+        />
+        <CardContent>
+          <Typography>Text</Typography>
+        </CardContent>
+      </Card>
       <ActionButtons
         previousProps={{ disabled: false }}
         nextProps={{
@@ -82,8 +86,8 @@ const Confirmation = () => {
           customText: "Send report"
         }}
         backLink="/more-info"
-        nextLink="/"
-        sendReport={Pages.WORKERS_AFFECTED}
+        nextLink="/thank-you"
+        sendReport={Pages.THANK_YOU}
         previousPage={Pages.MORE_INFO}
       />
     </>
